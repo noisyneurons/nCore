0497cbbee5095acda7b847fa75b7c643db4c803f
Mark White <com@markwhite.name>
2013-06-20 18:39:28 -0400

    A single Momentum component for each link (that includes both local flocking Error  and output error components) seems to work well.  Just finished modifying membership code for vector lengths == 1 -- so that distal examples are assigned a membership of 1.0 to the nearest cluster.

--- lib/core/WeightedClustering.rb
+++ lib/core/WeightedClustering.rb
@@ -26,7 +26,9 @@ class DynamicClusterer
   attr_reader :args, :numberOfClusters, :m, :delta, :maxNumberOfClusteringIterations,
               :numExamples, :exampleVectorLength, :minDistanceAllowed
 
+
   public
+
   def initialize(args)
     @args = args
     @minDistanceAllowed = args[:minDistanceAllowed] ||= 1.0e-10
@@ -120,9 +122,36 @@ class DynamicClusterer
   end
 
   def calcThisPointsFractionalMembershipToThisCluster(thePoint, arbitraryCluster, power) # TODO this code is doubly redundant.  It repeats the same calculations for each cluster.  Will be particularly inefficient for more than 2  clusters.
-    sumOfRatios = 0.0
-    #std("thePoint=\t",thePoint)
+    case exampleVectorLength
+      when 1
+        forVectorLengthEq1(arbitraryCluster, power, thePoint)
+      else
+        forVectorLengthGT1(arbitraryCluster, power, thePoint)
+    end
+  end
+
+
+  def forVectorLengthEq1(arbitraryCluster, power, thePoint)
+
+    case arbitraryCluster.center[0] >= 0.0
+      when true # cluster's center is on positive side
+        arbitraryDistance = arbitraryCluster.center[0] - thePoint[0]
+        return 1.0 if (arbitraryDistance < 0.0)
+        return pointsMembershipToArbitraryCluster(arbitraryDistance, power, thePoint)
+      when false # cluster's center is on negative side
+        arbitraryDistance = thePoint[0] - arbitraryCluster.center[0]
+        return 1.0 if (arbitraryDistance < 0.0)
+        return pointsMembershipToArbitraryCluster(arbitraryDistance, power, thePoint)
+    end
+  end
+
+  def forVectorLengthGT1(arbitraryCluster, power, thePoint)
     arbitraryDistance = arbitraryCluster.center.dist_to(thePoint)
+    pointsMembershipToArbitraryCluster(arbitraryDistance, power, thePoint)
+  end
+
+  def pointsMembershipToArbitraryCluster(arbitraryDistance, power, thePoint)
+    sumOfRatios = 0.0
     clusters.each do |comparisonCluster|
       comparisonDistance = comparisonCluster.center.dist_to(thePoint)
       comparisonDistance = [comparisonDistance, minDistanceAllowed].max # puts floor on comparison distance to avoid "divide by zero"
