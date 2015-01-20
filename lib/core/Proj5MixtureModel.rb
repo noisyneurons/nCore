89a1f6597402ba2f3ef9ad014ebca1424bf9ff6a
Mark White <com@markwhite.name>
2015-01-20 18:32:13 -0500

    Finished running 4 diff simulations: 2 with 30 degree rotations, with and without integration of training examples across 2nd layer of network.  Signifiicant difference between 30 degree and 0 degrees as to training and test error.  This is understandable: 30 degrees puts most of the differentiating information into one of the 2 input variables.
    Separately, and most important, is the fact that, for both rotations, the test error was significantly better for Proj4.
    
    Have made some minor mods for operation with Docker and AWS execution.  Continuing with experiments using GitHub as code base for simulations on docker.
    
    Now adding simplest logger (StringIO and FileIO), that will feed into Redis, and then possibly mongo.  KEEP IT SIMPLE>>>
    
    getting urls and $currenthost doing the right thinigs... AGAIN and saving again  REMOTE redis storage working just fine now.
    
    Both Proj3 and Proj4 working with Redis and environment variables.
    
    *** All std Projects working:  BasicBPDemo, Proj3, Proj4, and Proj3comparedTo4  I'm about to start designing/coding the cat's meow.
    
    Just did a significant amount of reshuffling code across files.  Some renaming of .rb files, also.  All bins work...
    
    Woops, I need to reverse about an hours worth of editing in the Learning Strategy file.!!
    
    All std Projects working again:  I forgot the "self." in "self.netInput"!
    
    spent a little time with docker and redis -- to get persistence between rebuilds and reruns.
    
    Now testing the continuous use of "alwaysOn" server for Redis storage of simuluation results........
    
    Ok, the server and redis stuff is done for now.  Now I've been adding code for Symmetrical Models for Self-Org and Supervised.  Also considering stochastic switching of neuron outputs according to the probability that the example is from one of the two lobe (Gauss Models).  In other words, if an example is unlikely "from" the lobe models, the example should be "ignored by that neuron" -- or, the neuron should indicate that it "has NO OPINION"!!
    
    Made significant progress on mixture model for the distribuion of a neuron's netInput or outputs.  netInputs or outputs depends on... the learning strategy for effectively and adaptively incorporate sigmoid (and other) nonlinearities in the network's processing.
    
    Been testing using scratch file.  Debug is going quickly...  Having some numerical (complex number) issues... seems to be related to convergence and using better initial estimates...    Got pdf properly scaled.  Streaming estimate of std and mean seems to be working acceptably.  BUT need to test more.  Also need to make supposedly more stable (but memory hog) batch version for calculating std......
    
    Did quite a bit of renaming of mixture model strategy methods.  Created stable functional testing file for testing 3 of the classes...

---
+++ lib/core/Proj5MixtureModel.rb
@@ -0,0 +1,90 @@
+### VERSION "nCore"
+## ../nCore/bin/Proj5MixtureModel.rb
+
+# Specific Purpose for this experiment: Get NEXT simplest versions of self-org, context, AND combined with Supervised Learning, understood and "working."
+#   In this 4th project, we append a "relearning or re- self-org WITHOUT CONTEXT" just prior to the bp supervised training.
+#   Presumably this will improve the accuracy of the hyperplanes in the 2nd hidden layer, compared to those in proj 3.
+
+# General Purpose:  Start of Project 4; project to split example set to learn sub-parts, and then combine those parts/neuron-functions that
+# didn't need to be separated, but instead need to be integrated to obtain better generalization.
+# Ultimate goal is develop analogy processing -- where one function useful for solving one problem
+# can be of use in solving another "similar-but-different" problem.  The common function(s)/neuron(s) can be thus be 'reused' -- and even potentially made
+# better by improving the accuracy of the function parameters because more examples are used to learn the parameters. ala Bayes
+
+require_relative '../lib/core/Utilities'
+require_relative '../lib/core/DataSet'
+require_relative '../lib/core/NeuralParts'
+require_relative '../lib/core/Layers'
+require_relative '../lib/core/NetworkFactories'
+require_relative '../lib/core/NetworkFactories2'
+require 'distribution'
+require_relative '../lib/core/NeuronLearningStrategies'
+require_relative '../lib/core/Trainers'
+
+require_relative '../lib/plot/CorePlottingCode'
+
+require_relative 'BaseLearningExperiment'
+########################################################################
+
+logger = StringIO.new
+
+args = {
+    :experimentNumber => $globalExperimentNumber,
+    :descriptionOfExperiment => "Proj5MixtureModel; single neuron test of mixture model and related learning strategy components",
+    :baseRandomNumberSeed => 0,
+
+    :classOfTheNetwork => OneNeuronNetwork,
+    :classOfTheTrainer => OneNeuron,
+    :classOfDataSetGenerator => OneNeuronDistributionDataSet,
+
+    # training parameters
+    :learningRate => 0.1,
+    :minMSE => 0.0,
+    :epochsForSelfOrg => 150, #150,
+    :trainingSequence => TrainingSequence,
+
+    # Network Architecture
+    :numberOfInputNeurons => 2,
+    :numberOfOutputNeurons => 1,
+
+    # Neural Parts Specifications
+    :typeOfLink => LinkWithNormalization,
+    :typeOfNeuron => Neuron2,
+    :typeOfLinkToOutput => LinkWithNormalization,
+    :typeOfOutputNeuron => OutputNeuron2,
+
+    :weightRange => 0.1,
+
+    # Training Set parameters
+    :numberOfExamples => 16,
+    :numberOfTestingExamples => 160,
+    :standardDeviationOfAddedGaussianNoise => 0.0,
+    :verticalShift => 0.0,
+    :horizontalShift => 0.0,
+    :angleOfClockwiseRotationOfInputData => 0.0,
+
+    # Results and debugging information storage/access
+    :logger => logger
+}
+
+
+###################################### REPEATED Experiments for comparison ##########################################
+
+numberOfRepetitions = 1
+
+runner = ExperimentRunner.new(args)
+lastExperimentRun, results = runner.repeatSimulation(numberOfRepetitions)
+# logger.puts lastExperimentRun.network
+
+loggedData = logger.string
+
+$redis.rpush("SimulationList", loggedData)
+$redis.save
+
+retrievedData = $redis.rpoplpush("SimulationList", "SimulationList")
+
+puts retrievedData
+
+numberOfExperimentsStoredInList = $redis.llen("SimulationList")
+
+puts "\n\nnumber Of Experiments Stored In List =\t#{numberOfExperimentsStoredInList}"
